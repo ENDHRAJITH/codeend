ededc4cefd936d6c9f2cb4f4ef457a7df30b23a5
Endhrajith T <endhrajitthiyagu@gmail.com>
2025-06-27 12:23:48 +0530

    new change:

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const port = 3000;                  // Define the port the server will listen on
 app.get('/', (req, res) => {
   res.send('Hello from Express.js!'); // Send a plain text response
 });
-
+console.log(
+"Endhrajith"
+)
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Express server running at http://localhost:${port}`);
